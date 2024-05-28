9941ed967e3074c24771d6b95e76a6dc027be64a
Jonathan <coding.with.johnny.dev@gmail.com>
2024-05-27 19:19:08 -0600

    feat: implement global error handling in API
    Have an understanding of:
    
    * API Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 // cors
 app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
